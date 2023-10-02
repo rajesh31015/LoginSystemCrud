5c3d06a4099ab5449541afb3e077ed42b28717a4
rajesh <rajesh@cwebconsultants.com>
2023-10-02 15:02:41 +0530

    changes

--- notes.js
+++ notes.js
@@ -13,3 +13,26 @@ server.use((request,response,next)=>{
       });
     }
 });
+
+
+
+const validationSchema = Joi.object({
+  occasion: Joi.string().required(),
+  no_of_words: Joi.number().required().min(20).max(50),
+  category: Joi.string().required(),
+  type:Joi.string().required(),
+  language:Joi.string().required(),
+  message: Joi.string().allow('').custom((value, helpers) => {
+      if (value && (value.includes('<script>') || value.includes('<') )) {
+        return helpers.error('string.dangerousContent');
+      }
+      const wordCount = value.trim().split(/\s+/).length;
+      if(wordCount > 30){
+          return helpers.error('string.length');
+      }
+      return value; // Value is valid
+  }),
+}).messages({
+  'string.dangerousContent': 'The message contains dangerous content and is not allowed.',
+  'string.length': 'Maximum 30 words allowed.'
+});
