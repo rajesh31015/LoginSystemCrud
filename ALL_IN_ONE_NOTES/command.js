d8c549d79e5c4bb21964ee667cb920488df76169
rajesh31015 <rajesh31015@gmail.com>
2024-04-02 22:15:51 +0530

    changes

--- ALL_IN_ONE_NOTES/command.js
+++ ALL_IN_ONE_NOTES/command.js
@@ -19,3 +19,20 @@
 // npm i next-auth -> google, facebook and custom
 // npm i naniid / used for unique key generate
 // npm i react-razorpay
+
+// ## Python Django RestFramework ##
+// django-admin startproject <project-name>
+// python manage.py runserver  // it run project with default port
+// python manage.py runserver <port> // it run project with specified port where port is 3000 just like that.
+// python manage.py startapp home where home ia a <app-name> ex. about,video etc.
+//  ## start djangorestFramework ##
+// pip install djangorestframework
+// pip install pipenv   / it is a virtual enviroment
+// pipenv shell / create a file that declares pacakge info
+// pipenv install django
+// pipenv install djangorestframework
+// django-admin startproject backend
+// django-admin startproject backend .  / if you want to not create a backend folder within the backend folder then write . at the end
+// python manage.py makemigrartions
+// python manage.py migrate
+// python manage.py create superuser
---
+++ ALL_IN_ONE_NOTES/python Django/generalNotes.md
@@ -0,0 +1,6 @@
+1. when create app running this django-admin startproject Varnz here Varnz is called root app.
+2. then create custom app to handle functionality just like routes in nodejs after that 
+3. to declare custom app to root app # settings.py->INSTALLED_APP [] array.
+4. we can give (-) symbol between the project name.
+5. installed djangorestframework after installed it it declare just like app diclare.
+6. pip install package and pipenv install pacakge both are different.
